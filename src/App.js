a4edf373d3f5ddc3d5fa4bf6aad5ed40e1adbf71
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-02-14 19:26:45 +0530

    stationary added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import Sweetshop from "./sweetshop.jpg"
+import Stationary from "./stationary.jpg"
 import { WEBSITE_NAME,WEBSITE_DESCRIPTION,SHOP_IMG,BUTTON1,BUTTON2,BUTTON_STYLE } from './config'
 
 export default function App() {
   return (
     <div>
-        <h1 className='text-center text-red-300 text-5xl my-7 underline decoration-dotted'>{WEBSITE_NAME}</h1>
+        <h1 className='text-center text-blue-300 text-5xl my-7 underline decoration-dotted'>{WEBSITE_NAME}</h1>
         <p className='text-center text-2xl mt-14 mx-16'>{WEBSITE_DESCRIPTION}</p>
         <img
-         src={Sweetshop}
+         src={Stationary}
          className='h-[300px] object-cover block mx-auto my-8'/>
 
         <div className='flex justify-center'>
-        <button className='bg-pink-800 text-white px-4 py-2 mx-4 rounded-md '>{BUTTON1}</button>
-        <button className='bg-pink-800 text-white px-4 py-2 mx-4 rounded-md'>{BUTTON2}</button>
+        <button className='bg-blue-950 text-white px-4 py-2 mx-4 rounded-md '>{BUTTON1}</button>
+        <button className='bg-blue-950 text-white px-4 py-2 mx-4 rounded-md'>{BUTTON2}</button>
         </div>
     </div>
   )
--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
-const WEBSITE_NAME='The Sweet Spot💗';
-const WEBSITE_DESCRIPTION="Welcome to The Sweet Spot💗 , where every bite is a taste of happiness! 🎂🍬 From rich chocolates and creamy desserts to traditional sweets and custom treats, we bring you the finest confections made with love and the freshest ingredients. Whether you're craving a sugary delight or looking for the perfect gift, we have something special for every occasion. Visit us today and indulge in sweetness like never before!";
+const WEBSITE_NAME='Ink & Paper';
+const WEBSITE_DESCRIPTION="Welcome to Ink & Paper, your one-stop destination for all things stationery! 🖊️📒 From premium notebooks and stylish pens to office essentials and art supplies, we have everything you need to bring your ideas to life. Whether you're a student, an artist, or a professional, our high-quality products will inspire creativity and productivity. Visit us today and explore our wide range of stationery essentials!";
 const BUTTON1='Explore';
 const BUTTON2='Order Now!!';
 const BUTTON_STYLE='{}';
-
 
 export {WEBSITE_NAME,WEBSITE_DESCRIPTION,BUTTON1,BUTTON2,BUTTON_STYLE};
